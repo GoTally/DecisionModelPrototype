138a9391181aa4b4bcc90bef39c7c9def6ce6c0b
Tyler Ewing <tewing10@gmail.com>
2014-11-24 22:31:16 -0500

    Add users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def index
+    render json: User.all, status: 200
+  end
+
+  def show
+    binding.pry
+    render json: User.find_by_id(params[:id]), status: 200
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: 200
+    else
+      render json: @user.errors, status: 422
+    end
+  end
+
+private
+
+  def user_params
+    params.permit(:first_name, :last_name)
+  end
+end
