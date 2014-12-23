803c7ac578f155f2ce51563eae89eee7f4f6abcd
Tyler Ewing <tewing10@gmail.com>
2014-12-23 15:19:20 -0500

    Create db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+Vote.delete_all
+Poll.delete_all
+Voter.delete_all
+Choice.delete_all
+
+user = User.create!(first_name: 'Test', last_name: 'User', age: 99, gender: 'undecided')
+poll = Poll.create!(title: 'Test poll', description: 'Test poll for Test User', status: 'new', creator_user_id: user.id)
+Choice.create!(value: 'red', poll_id: poll.id)
+Choice.create!(value: 'green', poll_id: poll.id)
+Choice.create!(value: 'blue', poll_id: poll.id)
+Voter.create!(poll_id: poll.id, user_id: user.id)
